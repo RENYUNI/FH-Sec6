53eb59a61997ce25e9618b2db743e66bb3612598
brautik <brau.tik.dev@gmail.com>
2020-10-21 19:19:42 -0500

    Creando una lista de categorias: se creó una lista JSX utilizando array.map(), luego se creó un botón cuyo onClick llama a la función agregar que actualiza el estado de categorias mediante setCategorias de useState, agregando un nuevo elemento con el operador spread

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 const GifExpertApp = props => {
+
+    const [categorias, setCategorias] = useState( ['One Punch', 'Samurai X', 'Dragon Ball'])
+
+    const agregar = () =>{
+        setCategorias( arg => [ ...arg, 'Inuyasha' ])
+    }
+
     return (
         <>
            <h2>GifExpertApp</h2> 
            <hr/>
+
+            <button onClick={ agregar }>Agregar</button>
+
+            <ol>
+               { categorias.map(e => <li key={ e }>{ e }</li>)}
+            </ol>
         </>
     )
 }
