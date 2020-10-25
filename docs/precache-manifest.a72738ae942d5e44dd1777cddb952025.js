70d410f4d5b5d4a0357c0bc0b9ba651e28481d72
brautik <brau.tik.dev@gmail.com>
2020-10-25 13:31:55 -0500

    Se renombró la carpeta ignorada build por docs para desplegar el proyecto en Github pages

---
+++ docs/precache-manifest.a72738ae942d5e44dd1777cddb952025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb90959b157a5b14e2a7ea2a8ecfafdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985b949d1a9e49c49a3c",
+    "url": "/static/css/main.40c0cfc2.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985b949d1a9e49c49a3c",
+    "url": "/static/js/main.b9eba610.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
